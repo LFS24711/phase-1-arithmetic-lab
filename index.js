a6583a75dbb8ceb18fc59359ba86c4d77d88cb25
Logan <loganfranklinsmith@hotmail.com>
2021-11-28 03:01:50 -0500

    commit tree 80f8d8828c74146be20bc561db0b843837434a10

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 14;
+const num4 = 5;
+const multiply = multiplication();
+const random = Math.floor(Math.random() * 10) +1;
+const mod = num3 % num4;
+const max = Math.max(5, 10, 15, 20)
+
+function multiplication() {
+        return num1 * num2;
+}
+
